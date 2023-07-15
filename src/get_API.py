2da836ab82cbf08ba4ac2d21de94bf9fd10e7f20
sergei valdemarov <valdemarovsergei@gmail.com>
2023-07-15 23:04:07 +0300

    get seve json.py

--- src/get_API.py
+++ src/get_API.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 import requests
 from vacancies import Vacancies
+from seve_json import JSON_save
 
 
 class API(ABC):
@@ -47,3 +48,26 @@ class HHAPI(API):
             work_place = vacancy.get("area").get("name")
             salary_from = vacancy.get("salary").get("from")
 
+            if salary_from is None:
+                salary_from = 0
+
+            salary_to = vacancy.get("salary").get("to")
+
+            if salary_to is None:
+                salary_to = 0
+
+            salary_currency = vacancy.get("salary").get("currency")
+            experience = vacancy.get("experience").get("name")
+
+            vac = Vacancies(title, vacancy_url, vacancy_id, company_name, work_place,
+                            salary_from, salary_to, salary_currency, experience)
+            print("----------------------------------------------------------------------------------------------")
+            print(vac)
+            user_answer = input("Добавить вакансию? (Да/Нет) ").lower()
+            if user_answer == "да" or user_answer == "yes" or user_answer == "lf":
+                my_object = JSON_save()
+                my_object.add_vacancy(vac.info)
+            elif user_answer == "стоп" or user_answer == "stop":
+                break
+            else:
+                continue
