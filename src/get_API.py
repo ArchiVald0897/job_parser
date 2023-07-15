d8740898d2575e49846c543691fb5967af89a247
sergei valdemarov <valdemarovsergei@gmail.com>
2023-07-15 22:25:36 +0300

    get class HHAPI(API)

--- src/get_API.py
+++ src/get_API.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import requests
 
 
 class API(ABC):
@@ -12,3 +13,24 @@ class API(ABC):
     def get_info(self, response):
         """Получение информации по вакансии"""
         pass
+
+
+class HHAPI(API):
+    """Поиск вакансий на HeadHunter"""
+    hh_url = 'https://api.hh.ru/vacancies'
+
+    def search_vacancies(self, keyword, quantity=15, page=1):
+        """Подключение к API HeadHunter"""
+        url = "https://api.hh.ru/vacancies"
+        params = {
+            "text": keyword,
+            "per_page": quantity,
+            "only_with_salary": 'true'
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            vacancies = response.json().get("items")
+            return vacancies
+        else:
+            print(f"Не удалось выполнить запрос к API HeadHunter")
